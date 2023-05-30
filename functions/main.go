a22b79136ab5f962340db69168b5ce01fcc3072c
Rodrigo Erico <rodrigoerico25@gmail.com>
2023-05-29 23:36:49 -0300

    example of functions

---
+++ functions/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+// func sum(a int, b int) (int, string) {
+// 	// return a + b
+// 	simpleReturn := a + b
+// 	formattedReturn := fmt.Sprintf("%d + %d = %d", a, b, simpleReturn)
+// 	return simpleReturn, formattedReturn
+// }
+
+func subtraction(x int, z int) (simpleReturn int, formattedReturn string) {
+	defer fmt.Println("Retorna no fim da execução da função!")
+	fmt.Println("Inicio da Função!")
+	simpleReturn = x - z
+	formattedReturn = fmt.Sprintf("%d + %d = %d", x, z, simpleReturn)
+	return
+}
+
+func main() {
+	simpleReturnResult, formattedReturnResult := subtraction(12, 10)
+	fmt.Println(simpleReturnResult)
+	fmt.Println(formattedReturnResult)
+	fmt.Printf("O tipo de dado do formattedReturnResult é uma %T.", formattedReturnResult)
+
+	// _, formattedReturnResult:= sum(12, 10)
+	// fmt.Println(simpleReturnResult)
+	// fmt.Println(formattedReturnResult)
+	// fmt.Printf("O tipo de dado do formattedReturnResult é uma %T.", formattedReturnResult)
+}
